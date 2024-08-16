c54a3ad0f5199c28b89036decbbe4a9e06ec3c75
Jackson Ming Hu <huming2207@gmail.com>
2024-08-16 23:17:09 +1000

    Rework packet encoding

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -1,65 +1,12 @@
-use core::cmp;
+use crate::constants::{
+    CacheQueue, SLIP_END, SLIP_ESC, SLIP_ESC_END, SLIP_ESC_ESC, SLIP_ESC_START, SLIP_START,
+};
 
-use stm32wlxx_hal::subghz::LoRaPacketStatus;
-
-use crate::constants::{CacheQueue, CRC, SLIP_END, SLIP_ESC, SLIP_ESC_END, SLIP_ESC_ESC, SLIP_ESC_START, SLIP_START};
-
-pub mod uart_packet;
-
-pub fn enqueue_radio_recv_pkt(pkt_status: LoRaPacketStatus, buf: &[u8], queue: &mut CacheQueue, data_len: u8) {
-    defmt::info!("radio: RxDone, got {:?}; len={}", pkt_status, data_len);
-    let mut digest = CRC.digest();
-    match queue.enqueue(SLIP_START) {
-        Ok(_) => {},
-        Err(b) => {
-            defmt::warn!("radio: Rx buffer full! Ditching oldest");
-            queue.dequeue(); // Drop the oldest
-            queue.enqueue(b).unwrap();
-        }
-    }
-
-    let pkt_rssi = cmp::max(pkt_status.signal_rssi_pkt().to_integer(), 0);
-    digest.update(&[(pkt_rssi * -1) as u8]);
-
-    let snr = cmp::max(pkt_status.snr_pkt().to_integer(), 0);
-    digest.update(&[(snr * -1) as u8]);
-
-    for b in buf {
-        digest.update(&[*b]);
-    }
-
-    let checksum: [u8; 2] = digest.finalize().to_le_bytes();
-    let pkt_len: [u8; 2] = ((checksum.len() + data_len as usize + 2) as u16).to_le_bytes();
-    slip_enqueue(queue, pkt_len[0]);
-    slip_enqueue(queue, pkt_len[1]);
-    slip_enqueue(queue, checksum[0]);
-    slip_enqueue(queue, checksum[1]);
-    
-    slip_enqueue(queue, (pkt_rssi * -1) as u8);
-    slip_enqueue(queue, (snr * -1) as u8);
-
-    for b in buf {
-        slip_enqueue(queue, *b);
-    }
-
-    match queue.enqueue(SLIP_END) {
-        Ok(_) => {},
-        Err(b) => {
-            defmt::warn!("radio: Rx buffer full! Ditching oldest");
-            queue.dequeue(); // Drop the oldest
-            queue.enqueue(b).unwrap();
-        }
-    }
-
-}
-
-pub fn parse_uart_packet(queue: &mut CacheQueue) -> Result<(), ()> {
-    Ok(())
-}
+pub mod uart_pkt_encoder;
 
 fn enqueue_ditch_oldest(queue: &mut CacheQueue, b: u8) {
-    match queue.enqueue(SLIP_START) {
-        Ok(_) => {},
+    match queue.enqueue(b) {
+        Ok(_) => {}
         Err(b) => {
             queue.dequeue(); // Drop the oldest
             queue.enqueue(b).unwrap();
@@ -72,15 +19,15 @@ fn slip_enqueue(queue: &mut CacheQueue, b: u8) {
         SLIP_START => {
             enqueue_ditch_oldest(queue, SLIP_ESC);
             enqueue_ditch_oldest(queue, SLIP_ESC_START);
-        },
+        }
         SLIP_ESC => {
             enqueue_ditch_oldest(queue, SLIP_ESC);
             enqueue_ditch_oldest(queue, SLIP_ESC_ESC);
-        },
+        }
         SLIP_END => {
             enqueue_ditch_oldest(queue, SLIP_ESC);
             enqueue_ditch_oldest(queue, SLIP_ESC_END);
-        },
+        }
         _ => {
             enqueue_ditch_oldest(queue, b);
         }
@@ -105,14 +52,14 @@ fn slip_dequeue(queue: &mut CacheQueue, out: &mut [u8]) -> Result<(), ()> {
                     // Something fucked, handle error
                     defmt::error!("slip_decode: Start byte occur twice!");
                 }
-            },
+            }
 
             SLIP_END => {
                 if begin {
                     begin = false;
                     break;
                 }
-            },
+            }
 
             SLIP_ESC => {
                 let esc = match queue.dequeue() {
@@ -124,15 +71,15 @@ fn slip_dequeue(queue: &mut CacheQueue, out: &mut [u8]) -> Result<(), ()> {
                     SLIP_ESC_END => {
                         out[ctr] = SLIP_END;
                         ctr += 1;
-                    },
+                    }
                     SLIP_ESC_START => {
                         out[ctr] = SLIP_START;
                         ctr += 1;
-                    },
+                    }
                     SLIP_ESC_ESC => {
                         out[ctr] = SLIP_ESC;
                         ctr += 1;
-                    },
+                    }
                     _ => {
                         defmt::error!("slip_decode: unknown ESC byte {:02x}", esc);
                         break;
@@ -158,4 +105,3 @@ fn slip_dequeue(queue: &mut CacheQueue, out: &mut [u8]) -> Result<(), ()> {
         Err(())
     }
 }
-
---
+++ src/packet/uart_pkt_encoder.rs
@@ -0,0 +1,78 @@
+use core::cmp;
+
+use crc::Digest;
+use stm32wlxx_hal::subghz::LoRaPacketStatus;
+
+use crate::constants::{CacheQueue, PacketType, CRC, SLIP_END, SLIP_START};
+
+use super::slip_enqueue;
+
+pub struct UartPacketEncoder<'a> {
+    queue: &'a mut CacheQueue,
+    digest: Digest<'a, u16>,
+}
+
+impl<'a> UartPacketEncoder<'a> {
+    pub fn new(pkt_type: PacketType, queue: &'a mut CacheQueue) -> UartPacketEncoder<'a> {
+        let mut digest = CRC.digest();
+        digest.update(&[pkt_type as u8]);
+
+        match queue.enqueue(SLIP_START) {
+            Ok(_) => {}
+            Err(b) => {
+                defmt::warn!("UartPacket: Rx buffer full! Ditching oldest");
+                queue.dequeue(); // Drop the oldest
+                queue.enqueue(b).unwrap();
+            }
+        }
+
+        UartPacketEncoder { queue, digest }
+    }
+
+    pub fn add_packet_len(&mut self, pkt_len: usize) {
+        let pkt_len_bytes: [u8; 2] = (pkt_len as u16).to_le_bytes();
+        slip_enqueue(self.queue, pkt_len_bytes[0]);
+        slip_enqueue(self.queue, pkt_len_bytes[1]);
+    }
+
+    pub fn add_payload_with_lora_status(
+        &mut self,
+        payload: &[u8],
+        data_len: u8,
+        pkt_status: LoRaPacketStatus,
+    ) {
+        let pkt_rssi = cmp::max(pkt_status.signal_rssi_pkt().to_integer(), 0);
+        self.digest.update(&[(pkt_rssi * -1) as u8]);
+
+        let snr = cmp::max(pkt_status.snr_pkt().to_integer(), 0);
+        self.digest.update(&[(snr * -1) as u8]);
+
+        for b in payload {
+            self.digest.update(&[*b]);
+        }
+
+        self.add_packet_len((2 + data_len as usize + 2) as usize); // 2 bytes of CRC, 2 bytes of RSSI and SNR, plus data length
+
+        slip_enqueue(self.queue, (pkt_rssi * -1) as u8);
+        slip_enqueue(self.queue, (snr * -1) as u8);
+
+        for b in payload {
+            slip_enqueue(self.queue, *b);
+        }
+    }
+
+    pub fn finalize(self) {
+        let checksum: [u8; 2] = self.digest.finalize().to_le_bytes();
+        slip_enqueue(self.queue, checksum[0]);
+        slip_enqueue(self.queue, checksum[1]);
+
+        match self.queue.enqueue(SLIP_END) {
+            Ok(_) => {}
+            Err(b) => {
+                defmt::warn!("radio: Rx buffer full! Ditching oldest");
+                self.queue.dequeue(); // Drop the oldest
+                self.queue.enqueue(b).unwrap();
+            }
+        }
+    }
+}
