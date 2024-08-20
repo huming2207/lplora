27f89eac9653ec4fca0c453bae84b541c54037ec
Jackson Ming Hu <huming2207@gmail.com>
2024-08-20 20:57:04 +1000

    constant: add sleep config

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 use heapless::spsc::Queue;
-use stm32wlxx_hal::gpio;
+use stm32wlxx_hal::{gpio, subghz::{SleepCfg, Startup}};
 
 pub const RFSW_GPIO_OUTPUT_ARGS: gpio::OutputArgs = gpio::OutputArgs {
     level: gpio::PinState::Low,
@@ -15,3 +15,4 @@ pub const SLIP_ESC_END: u8 = 0xdc;
 pub const SLIP_ESC_ESC: u8 = 0xdd;
 pub const SLIP_ESC_START: u8 = 0xde;
 pub type CacheQueue = Queue<u8, 1024>;
+pub const SLEEP_CFG: SleepCfg = SleepCfg::new().set_rtc_wakeup_en(false).set_startup(Startup::Cold);
