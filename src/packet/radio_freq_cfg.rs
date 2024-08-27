7777e0caa941366bff28ffae68f39e8fff31263d
Jackson Ming Hu <huming2207@gmail.com>
2024-08-28 08:53:00 +1000

    Add more logs

--- src/packet/radio_freq_cfg.rs
+++ src/packet/radio_freq_cfg.rs
@@ -22,7 +22,7 @@ impl TryFrom<UartPacketDecoder> for RadioFreqConfigurator {
 
         let freq_hz = u32::from_le_bytes(buf[0..=3].try_into().unwrap());
         if freq_hz < (100 * 1000000) || freq_hz > (960 * 1000000) {
-            defmt::error!("RadioFreqConfigurator: frequency out of range! freq_hz={}", freq_hz);
+            defmt::error!("RadioFreqConfigurator: frequency out of range! freq_hz={}; 0x{:x} 0x{:x} 0x{:x} 0x{:x}", freq_hz, buf[0], buf[1], buf[2], buf[3]);
             return Err(UartPacketError::CorruptedError);
         }
 
@@ -38,6 +38,7 @@ impl RadioFreqConfigurator {
         let freqx4 = mhz - (mhz % 4);
         radio.calibrate_image(CalibrateImage::from_freq((freqx4 - 4) as u16, (freqx4 + 4) as u16))?;
 
+        defmt::info!("RadioFreqConfigurator: config OK, freq={:?}", self.freq_hz);
         Ok(())
     }
 }
